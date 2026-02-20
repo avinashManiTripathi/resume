7803060e7227871d6ef6bfc1f4fba135714bee8f
Avinash Mani Tripathi <avinashmanitripathi@Mac.lan>
2026-02-20 21:09:46 +0530

    fixed next config

--- apps/landing/next.config.ts
+++ apps/landing/next.config.ts
@@ -76,6 +76,10 @@ const nextConfig: NextConfig = {
             key: 'Cache-Control',
             value: 'public, max-age=31536000, immutable',
           },
+          {
+            key: 'X-Robots-Tag',
+            value: 'noindex',
+          },
         ],
       },
       {
